9a97448f8cf0eba858089d2d115dd5f85f5cfcce
Tyler Antoni <79325792+Dibble710@users.noreply.github.com>
2021-07-18 10:13:28 -0400

    First Commit (basic app created)

---
+++ script.js
@@ -0,0 +1,18 @@
+function playAudio(e) {
+  const audio = document.querySelector(`audio[data-key="${e.key}"]`);
+  const key = document.querySelector(`.key[data-key="${e.key}"]`);
+  
+  if (!audio) return;
+  audio.currentTime = 0; // Rewind the audio clip to prevent delay
+  audio.play();
+  
+  key.classList.add(`playing`);
+}
+function removeTransition(e) {
+    if (e.propertyName !== "transform") return;
+    this.classList.remove(`playing`);
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+window.addEventListener("keydown", playAudio);

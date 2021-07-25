8bbeb071755c7eea9cb140b3970e2d73635f6989
Tyler Antoni <79325792+Dibble710@users.noreply.github.com>
2021-07-24 23:02:37 -0400

    Finsished front end

--- Weather App/script.js
+++ Weather App/script.js
@@ -1,7 +1,12 @@
 const timezone = document.querySelector("[data-timezone]");
 window.addEventListener("load", () => {
-  let long;
+  let lon;
   let lat;
+  let tempDescription = document.querySelector(".temp-description");
+  let locationTimezone = document.querySelector("[data-timezone]");
+  let feelsLike = document.querySelector(".feels-temp");
+  let actualTemp = document.querySelector(".actual-temp");
+  let windSpeed = document.querySelector('[data-wind-speed]')
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -19,8 +24,20 @@ window.addEventListener("load", () => {
         })
         .then((data) => {
           console.log(data);
-          const location = data.name
-          const temp = data.main.temp
+          const location = data.name;
+          const { temp, feels_like } = data.main;
+          const description = data.weather[0].description;
+          const weatherIcon = data.weather[0].icon
+          const wind = data.wind.speed;
+          console.log(weatherIcon);
+
+          // set DOM elements from API
+          actualTemp.textContent = temp + " F";
+          actualTemp.classList.remove('.loading')
+          feelsLike.textContent = feels_like + " F";
+          locationTimezone.textContent = location;
+          tempDescription.textContent = description;
+          windSpeed.textContent = `${wind.toString().substr(0,1)} mph`
         });
     });
   }

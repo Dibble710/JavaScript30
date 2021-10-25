49a6e49e5c77ee894099be3321e5d4dd471953ca
Dibble710 <tylerantoni1025@icloud.com>
2021-10-24 21:38:38 -0400

    styling changes

--- ParallaxScrolling/scripts.js
+++ ParallaxScrolling/scripts.js
@@ -10,10 +10,10 @@ let mountains_front = document.getElementById("mountains_front");
 window.addEventListener("scroll", function () {
   let value = window.scrollY;
   stars.style.left = value * 0.25 + "px";
-  moon.style.top = value * 1.05 + "px";
+  moon.style.top = value * 1.25 + "px";
   mountains_behind.style.top = value * 0.5 + "px";
-  mountains_front.style.top = 0 + "px";
-  text.style.marginRight = value * 4 + "px";
+  mountains_front.style.top = 0.1 + "px";
+  text.style.marginRight = value * 3 + "px";
   text.style.marginTop = value * 1.5 + "px";
   btn.style.marginTop = value * 1.5 + "px";
   header.style.top = value * 0.5 + "px";
